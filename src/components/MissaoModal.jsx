ec749ff8ef4a591f3edfbab253638b184fce3a4c
GabrielBoscoDeolindo <gabibosco01@gmail.com>
2025-10-10 13:40:18 -0300

    responsivade no modal

--- src/components/MissaoModal.jsx
+++ src/components/MissaoModal.jsx
@@ -20,7 +20,6 @@ export function MissaoModal({ missao, onClose, onConcluir }) {
       setResultado("Resposta correta! Parabéns!");
       setStatus("sucesso");
 
-      // ✅ chama a função de concluir após 1s (tempo para mostrar feedback)
       setTimeout(() => {
         onConcluir(missao.id);
       }, 1000);
@@ -31,33 +30,36 @@ export function MissaoModal({ missao, onClose, onConcluir }) {
   };
 
   return (
-    <dialog open className="modal" aria-labelledby="titulo-missao" aria-describedby="descricao-missao">
-      <h2 className="titulo" id="titulo-missao">
+    <dialog open className="modal w-[90%] md:max-w-md" aria-labelledby="titulo-missao" aria-describedby="descricao-missao">
+      <h2 className="titulo text-lg sm:text-xl md:text-2xl" id="titulo-missao">
         {missao.titulo}
       </h2>
-      <p id="descricao-missao">{missao.descricao}</p>
+      
+      <p id="descricao-missao" className="my-4 text-sm">
+        {missao.descricao}
+      </p>
 
-      <label htmlFor="resposta" className="sr-only">
+      <label htmlFor="resposta" className="sr-only sm:text-1">
         Digite sua resposta
       </label>
       <input
-        className="caixaTexto"
+        className="caixaTexto w-full"
         id="resposta"
         type="text"
-        placeholder="Digite sua resposta..."
+        placeholder="Digite aqui"
         value={resposta}
         onChange={(e) => setResposta(e.target.value)}
         required
       />
-
-      <div className="modal-botoes">
-        <button onClick={verificarResposta}>Enviar</button>
-        <button onClick={onClose}>Fechar</button>
+      
+      <div className="modal-botoes mt-4 flex flex-col gap-2 sm:flex-row-reverse">
+        <button onClick={verificarResposta} className="w-full sm:w-auto text-sm md:text-base">Enviar</button>
+        <button onClick={onClose} className="w-full sm:w-auto text-sm md:text-base">Fechar</button>
       </div>
 
       {resultado && (
-        <div className="resultado" role="alert">
-          <p>{resultado}</p>
+        <div className="resultado mt-4 flex flex-col items-center gap-2" role="alert">
+          <p className="text-sm md:text-base">{resultado}</p>
           {status === "sucesso" && (
             <img
               src={sucesso}
